ff57faff390e356358e1357ba7dabd1edfeb557a
Rayhan Mohammed <Rayhanmohammed40@gmail.com>
2024-05-27 01:32:21 +0530

    newchanges

--- add.c
+++ add.c
@@ -3,10 +3,10 @@
 int main() {
     int num1,num2,num3,num5,num6,num8; sum;
     
-    printf("Type the first number: ");
+    printf("Type the first num: ");
     scanf("%d", &num1);
     
-    printf("Type the second number: ");
+    printf("Type the second num: ");
     scanf("%d", &num2);
     
     sum = num1 + num2 +num3+num5+num6+num8;
